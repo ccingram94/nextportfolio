b37e5facc591755b2cfde250f85a73fa7ee86dab
ccingram94 <constanceingram94@gmail.com>
2021-03-18 12:38:35 -0500

    added about, resume, and projects pages

---
+++ pages/resume.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { motion } from 'framer-motion'
+
+export default function Resume() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Constance.io</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <video autoPlay muted loop id="bgvid">
+        <source src="record.mp4" type="video/mp4"></source>
+      </video>
+      <main id="main">
+      </main>
+    </div>
+  )
+}
